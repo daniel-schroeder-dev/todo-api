7132ce830c4478304b6234c8a95645c6737e4754
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-11 18:26:20 -0500

    Wipe all front-end files.

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-  app.use(require('morgan')('dev'));
-}
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 require('./db-connect');
 require('./models/todo');
@@ -15,16 +13,8 @@ const Todo = require('mongoose').model('Todo');
 
 const PORT = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
-
+app.use(morgan('dev'));
 app.use('/api/todos', todosRoutes);
-
-app.get('/', (req, res, next) => {
-  Todo.find()
-    .then(todos => res.render('index', { todos }))
-    .catch(console.error);
-});
 
 app.listen(PORT, () => {
   console.log('Express up at: ', PORT);
